e1f0f7da24db29dfdea1fdee4400c69082e8f16a
Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
2024-07-23 10:19:47 +0200

    Update if-eq.js

--- helpers/if-eq.js
+++ helpers/if-eq.js
@@ -1,4 +1,4 @@
-/* eslint-disable-next-line unicorn/prefer-module */
+/* eslint-disable-next-line unicorn/prefer-module, unicorn/no-anonymous-default-export */
 module.exports = function (Handlebars) {
 	Handlebars.registerHelper('if_eq', function (a, b, options) {
 		/* eslint-disable-next-line eqeqeq */
