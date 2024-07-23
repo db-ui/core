24ccad6f78bd1ae4d7a39fcec536c5db4ff5c307
Maximilian Franzke <787658+mfranzke@users.noreply.github.com>
2024-07-23 10:20:06 +0200

    Update unique-id.js

--- helpers/unique-id.js
+++ helpers/unique-id.js
@@ -1,4 +1,4 @@
-/* eslint-disable-next-line unicorn/prefer-module */
+/* eslint-disable-next-line unicorn/prefer-module, unicorn/no-anonymous-default-export */
 module.exports = function (Handlebars) {
 	Handlebars.registerHelper('unique-id', function () {
 		const crypto = require('node:crypto');
