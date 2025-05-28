a4b20237b53a6f2b437c8106bd7d57878739774d
Maximilian Franzke <Maximilian.Franzke@deutschebahn.com>
2025-05-28 19:23:33 +0200

    refactor: migrated config

---
+++ xo.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('xo').FlatXoConfig} */
+const xoConfig = [
+	{
+		ignores: [
+			'**/*.plugin.js',
+			'source/fonts/**/sources/',
+			'helpers/test.js',
+			'tests/backstop_data/engine_scripts/',
+			'out/**/*',
+			'dist/**/*',
+			'**/*.min.js'
+		],
+		prettier: true,
+		rules: {
+			'one-var': 0,
+			'no-use-before-define': 0,
+			'no-console': 'error'
+		}
+	}
+];
+
+export default xoConfig;
